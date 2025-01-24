93a8046df39362876a040a68e18ce9d758c8c494
rc-earnest <rc.earnest04@gmail.com>
2025-01-23 17:36:06 -0700

    adds the working code and comments

--- SayMyName/SayMyName.vb
+++ SayMyName/SayMyName.vb
@@ -1,7 +1,20 @@
-﻿Module SayMyName
+﻿'Rudy Earnest
+'RCET2265
+'Spring 2025
+'Say My Name
+'https://github.com/rc-earnest/SayMyName.git
+Option Explicit On
+Option Strict On
+
+Module SayMyName
 
     Sub Main()
-
+        Dim Name As String
+        Console.WriteLine("What is your name?")
+        Console.WriteLine()
+        Name = Console.ReadLine 'gets user input
+        Console.WriteLine()
+        Console.WriteLine(Name & "?!" & " Who was in charge of naming you human?") 'displays user input
     End Sub
 
 End Module
